367b3e05c1f77df7e08bd99e529cc08e23b1d834
Timothy Gack <tim.gack@solarfiretech.com>
2015-04-17 20:01:29 -0500

    Add production bill of material generator Remove design bill of material generator

---
+++ pcb/bom.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# 
+# Copyright (c) 2015
+# Solarfire Technologies, LLC
+#
+# Contact: tim.gack@solarfiretech.com
+#
+# Licensed under CERN OHL v.1.2 or later
+#
+# You may redistribute and modify this documentation under the terms of the
+# CERN OHL v.1.2. (http://ohwr.org/cernohl) or later . This documentation is 
+# distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING OF
+# MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE. Please see the CERN OHL v.1.2 or later for applicable conditions.
+#
+gnetlist -g bom2 -o sft-hw-0023.txt sft-hw-0023-1.sch sft-hw-0023-2.sch sft-hw-0023-3.sch sft-hw-0023-4.sch
